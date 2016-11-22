43b6b05415013d495f04c7c446ff5fd9da911500
Walton Lee <mr.waltonlee@me.com>
2016-11-22 22:03:32 +0000

    rolling back changes

--- print_number.c
+++ print_number.c
@@ -1,29 +1,6 @@
 #include "holberton.h"
 #include <stdarg.h>
 /**
- *checkformatter - checks to see if the identifier is equal to
- *what the array of structs is checking for and pointing to the correct
- *function
- *@args: The list of arguments taken in with different data types
- *@print_a: The array that contains the struct
- *@format: Specifies whcih identifier it's going to be
- *Return: either 0 if none of the identifiers match, chars if it does match
- */
-int checkformatter(va_list args, print_a_t print_a[], char format)
-{
-	int j, chars;
-
-	chars = 0;
-	for (j = 0; j < 5; j++)
-	{
-		if (*print_a[j].s == format)
-			chars += (print_a[j].f(args));
-		return (chars);
-	}
-	return (0);
-
-}
-/**
  *_puts - a function that prints out the string followed by a new line.
  *@str: a variable that contains the string.
  *
@@ -44,8 +21,8 @@ void _puts(char *str)
  */
 int print_per(int p)
 {
-	if (p == '%')
-		_putchar(p);
+	p = '%';
+	_putchar(p);
 	return (1);
 }
 /**
